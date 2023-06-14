ea45be39e43e4e14868231f54eff3d9a2d81bcee
artur9487 <artur.www9487@gmail.com>
2023-06-14 22:46:32 +0200

    Added new project

--- src/MainView.js
+++ src/MainView.js
@@ -1,21 +1,38 @@
 /** @format */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
+import { useState } from 'react';
 
 const MainView = () => {
 	const authors = ['Stephen King', 'Brandon Sanderson', 'Rowling Tolkien'];
+	const [data, setData] = useState([]);
 
-	const data = authors
-		.map((author) => author.replace(' ', '+'))
-		.map((author2) => {
-			return axios
-				.get(
-					`https://www.googleapis.com/books/v1/volumes?q=${author2}&key=AIzaSyBtCL1XBIUeV7VUj9Wb30tqUQ2KDwqKQNM`
-				)
-				.then((res) => console.log(res))
-				.catch((err) => console.log(err));
-		});
+	/*useEffect(() => {
+		authors
+			.map((author) => author.replace(' ', '+'))
+			.map((author2) => {
+				return axios
+					.get(
+						`https://www.googleapis.com/books/v1/volumes?q=inauthor:${author2}&key=AIzaSyBtCL1XBIUeV7VUj9Wb30tqUQ2KDwqKQNM+&maxResults=10`
+					)
+					.then((res) => {
+						setData(res.data.items);
+					})
+					.catch((err) => console.log(err));
+			});
+	}, [authors]);*/
+
+	useEffect(() => {
+		axios
+			.get(
+				`https://www.googleapis.com/books/v1/volumes?q=inauthor:Brandon+Sanderson&key=AIzaSyBtCL1XBIUeV7VUj9Wb30tqUQ2KDwqKQNM+&maxResults=10`
+			)
+			.then((res) => {
+				setData(res.data.items);
+			})
+			.catch((err) => console.log(err));
+	}, [authors]);
 
 	console.log(data);
 
@@ -23,23 +40,26 @@ const MainView = () => {
 		<table>
 			<thead>
 				<tr>
-					<th className='border border-slate-300 ...'>Ko</th>
-					<th className='border border-slate-300 ...'>City</th>
+					<th className='border border-slate-300 ...'>ID</th>
+					<th className='border border-slate-300 ...'>Author</th>
+					<th className='border border-slate-300 ...'>Title</th>
+					<th className='border border-slate-300 ...'>Kind</th>
 				</tr>
 			</thead>
 			<tbody>
-				<tr>
-					<td className='border border-slate-300 ...'>Indiana</td>
-					<td className='border border-slate-300 ...'>Indianapolis</td>
-				</tr>
-				<tr>
-					<td className='border border-slate-300 ...'>Ohio</td>
-					<td className='border border-slate-300 ...'>Columbus</td>
-				</tr>
-				<tr>
-					<td className='border border-slate-300 ...'>Michigan</td>
-					<td className='border border-slate-300 ...'>Detroit</td>
-				</tr>
+				{/*data.map((item) => {
+					const ID = item.accessInfo.id;
+					const Title = item.volumeInfo.title;
+					const Author = item.volumeInfo.authors;
+
+					return (
+						<tr>
+							<td className='border border-slate-300 ...'>{ID}</td>
+							<td className='border border-slate-300 ...'>{Author}</td>
+							<td className='border border-slate-300 ...'>{Title}</td>
+						</tr>
+					);
+				})*/}
 			</tbody>
 		</table>
 	);
