8fa33856d1ddb151dde0dbcba9c37a034e4ccef4
artur9487 <artur.www9487@gmail.com>
2023-06-20 21:08:51 +0200

    New Layout changes

--- src/BreadCrumb.js
+++ src/BreadCrumb.js
@@ -7,19 +7,38 @@ import useFetch from './CustomHook';
 const BreadCrumb = () => {
 	let { author } = useParams();
 	const newAuthor = author.replace(' ', '+');
-	const [data] = useFetch(newAuthor, 10);
+	const [data] = useFetch(newAuthor, 4);
 
 	return (
-		<div className='container mx-auto w-2 f-2 bg-gray-700'>
-			{data.map((item) => {
-				return item.map((item2, indx) => {
-					const ID = item2.id;
-					const Title = item2.volumeInfo.title;
-					const source = item2.volumeInfo.imageLinks;
-					console.log(source);
-					return <div key={indx}></div>;
-				});
-			})}
+		<div className='p-10  w-1/2 h-screen bg-gray-700 border-white-200 border-4'>
+			<p>Your 4 books</p>
+			<div className='grid grid-cols-2 gap-2 h-screen'>
+				{data.map((item) => {
+					return item.map((item2, indx) => {
+						const ID = item2.id;
+						const Title = item2.volumeInfo.title;
+						console.log(item2.volumeInfo.imageLinks);
+						const source =
+							item2.volumeInfo.imageLinks?.smallThumbnail;
+						return (
+							<div
+								className='border border-white-300 flex flex-col items-center justify-center'
+								key={indx}>
+								<div className='border border-white-300 basis-1/2 w-1/2'>
+									<img
+										className='w-full object-cover h-1/2'
+										alt='cover image'
+										src={source}
+									/>
+									<p className='p-3 text-center text-white'>
+										{Title}
+									</p>
+								</div>
+							</div>
+						);
+					});
+				})}
+			</div>
 		</div>
 	);
 };
--- src/MainView.js
+++ src/MainView.js
@@ -14,54 +14,61 @@ const MainView = () => {
 		navigate(`/${author}`);
 	};
 
-	return (
-		<div className='container flex items-center flex-col bg-gray-700 w-full h-screen border-2 border-indigo-400'>
-			<form className='flex flex-col py-4'>
-				<label htmlFor='name'>Search for an author</label>
-				<input
-					name='name'
-					type='text'
-					value={searchPattern}
-					onChange={(e) => setSetPattern(e.target.value)}
-				/>
-			</form>
-			<table className=' bg-gray-800 mx-auto rounded-lg text-white'>
-				<thead>
-					<tr className='border-b-2 border-indigo-400'>
-						<th>Lp</th>
-						<th>Author</th>
-						<th>Kind</th>
-					</tr>
-				</thead>
-				<tbody>
-					{data === undefined ? (
-						<tr>
-							<td className='border border-slate-300 ...'>"Loading"</td>
-						</tr>
-					) : (
-						data.map((item, index) => {
-							return item.map((item2, index2) => {
-								const Author = item2.volumeInfo.authors;
-								const Kind = item2.volumeInfo.categories;
+	console.log(data);
 
-								return (
-									<tr
-										className='border-b-2 border-indigo-400 py-5'
-										onClick={() => handleClick(Author)}
-										key={index2}>
-										<td>{index2 + 1}</td>
-										<td>{Author}</td>
-										<td>{Kind}</td>
-									</tr>
-								);
-							});
-						})
-					)}
-				</tbody>
-			</table>
-			<div>
-				<Outlet />
+	return (
+		<div className='container flex items-center flex-row bg-gray-700 w-full h-full border-2 border-indigo-400'>
+			<div className='w-1/2'>
+				<form className='flex flex-col py-4'>
+					<label className='font-medium text-white' htmlFor='name'>
+						Search for an author
+					</label>
+					<input
+						className='bg-gray-600'
+						name='name'
+						type='text'
+						value={searchPattern}
+						onChange={(e) => setSetPattern(e.target.value)}
+					/>
+				</form>
+				<table className=' mx-auto rounded-lg text-white'>
+					<thead>
+						<tr className='border-b-2 border-gray-800'>
+							<th>Lp</th>
+							<th>Author</th>
+							<th>Kind</th>
+						</tr>
+					</thead>
+					<tbody>
+						{data === undefined ? (
+							<tr>
+								<td className='border border-slate-300 ...'>
+									"Loading"
+								</td>
+							</tr>
+						) : (
+							data.map((item, index) => {
+								return item.map((item2, index2) => {
+									const Author = item2.volumeInfo.authors;
+									const Kind = item2.volumeInfo.categories;
+
+									return (
+										<tr
+											className='even:bg-gray-800 p-3 hover:bg-gray-600  py-5 cursor-pointer'
+											onClick={() => handleClick(Author)}
+											key={index2}>
+											<td className='py-2 px-2'>{index2 + 1}.</td>
+											<td className='py-2 px-2'>{Author}</td>
+											<td className='py-2 px-2'>{Kind}</td>
+										</tr>
+									);
+								});
+							})
+						)}
+					</tbody>
+				</table>
 			</div>
+			<Outlet />
 		</div>
 	);
 };
